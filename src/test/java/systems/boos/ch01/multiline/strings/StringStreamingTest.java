64b16f4f3ee4368ec42141a5df8d783267d2896e
Stefan Boos <kontakt@boos.systems>
2025-01-19 20:49:07 +0100

    style: prettier-maven-plugin richtig konfiguriert

--- src/test/java/systems/boos/ch01/multiline/strings/StringStreamingTest.java
+++ src/test/java/systems/boos/ch01/multiline/strings/StringStreamingTest.java
@@ -10,16 +10,14 @@ class StringStreamingTest {
 
   @Test
   void someTest() {
-    var result = Stream
-      .of(
-        "UPDATE \"public\".\"office\"",
-        "SET (\"address_first\", \"address_second\", \"phone\") =",
-        "  (SELECT \"public\".\"employee\".\"first_name\",",
-        "          \"public\".\"employee\".\"last_name\", ?",
-        "   FROM \"public\".\"employee\"",
-        "   WHERE \"public\".\"employee\".\"job_title\" = ?"
-      )
-      .collect(Collectors.joining(String.valueOf(System.lineSeparator())));
+    var result = Stream.of(
+      "UPDATE \"public\".\"office\"",
+      "SET (\"address_first\", \"address_second\", \"phone\") =",
+      "  (SELECT \"public\".\"employee\".\"first_name\",",
+      "          \"public\".\"employee\".\"last_name\", ?",
+      "   FROM \"public\".\"employee\"",
+      "   WHERE \"public\".\"employee\".\"job_title\" = ?"
+    ).collect(Collectors.joining(String.valueOf(System.lineSeparator())));
 
     assertEquals(StringConstants.SQL, result);
   }
